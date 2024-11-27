e7518715e8dbb6886ddd573bf383841c967efabd
ArturM2005 <arturmuhamediarov@gmail.com>
2024-11-27 09:17:37 +0300

    created route to get all notebooks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Notebook;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('api/v1/notebook', function () {
+    return Notebook::all();
 });
