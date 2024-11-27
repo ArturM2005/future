c4d82e014b4691555922704f18be98030a5b4435
ArturM2005 <arturmuhamediarov@gmail.com>
2024-11-27 23:48:34 +0300

    created notebook controller

--- app/Http/Controllers/NotebookController.php
+++ app/Http/Controllers/NotebookController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notebook;
 use Illuminate\Http\Request;
 
 class NotebookController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Notebook::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Notebook $notebook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Notebook $notebook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Notebook $notebook)
+    {
+        //
+    }
 }
