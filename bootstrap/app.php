e65eb8f4cd6997b078b57186cc23a12d233b3d19
ArturM2005 <arturmuhamediarov@gmail.com>
2024-11-28 16:00:31 +0300

    Turned off csrf protection for testing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'https:versera.ru/api/v1/notebooks',
+            'https:versera.ru/api/v1/notebooks/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
